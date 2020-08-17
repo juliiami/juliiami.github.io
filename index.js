a8b21c5984cba29e6c77a62d6a8bc0a9f32eb19e
juliiami <45482942+juliiami@users.noreply.github.com>
2020-08-17 16:07:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 import * as tf from '@tensorflow/tfjs';
 
 const CLASSES = {
@@ -23,10 +6,7 @@ const CLASSES = {
   2: 'photo'
 }
 
-const MOBILENET_MODEL_PATH =
-    // tslint:disable-next-line:max-line-length
-    //'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
-	'/model/model.json'
+const MOBILENET_MODEL_PATH = '/model/model.json'
 
 const IMAGE_SIZE = 224;
 const TOPK_PREDICTIONS = 1;
