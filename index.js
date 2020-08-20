29aa20282628be1cc50f72d2ec74c9a30f247a04
juliiami <45482942+juliiami@users.noreply.github.com>
2020-08-20 17:50:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var model = undefined;
 
 
 async function initialize() {
-    model = await tf.loadLayersModel('/model_3_epochs/model.json');
+    model = await tf.loadLayersModel('/model3/model.json');
 }
 
 async function predict() {
