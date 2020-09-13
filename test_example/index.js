200bf346d948db89d0c761387c8d5341b15d61a7
juliiami <45482942+juliiami@users.noreply.github.com>
2020-09-13 19:38:45 +0300

    Update index.js

--- test_example/index.js
+++ test_example/index.js
@@ -17,7 +17,7 @@ async function loadModel() {
 	load_button = document.getElementById("load-button");
 	loader.style.display = "block";
 	model = undefined;
-	model = await tf.loadLayersModel('https://juliiami.github.io/model1/model.json');
+	model = await tf.loadLayersModel('https://juliiami.github.io/mobilenet/model.json');
 	loader.style.display = "none";
 	load_button.disabled = true;
 	load_button.innerHTML = "Loaded Model";
